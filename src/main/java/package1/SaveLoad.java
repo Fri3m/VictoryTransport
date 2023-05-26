14211d66ec1566a59b2896e3a700471402255932
Fri3m <omerisomer@outlook.com>
2023-05-26 16:41:50 +0300

    Third Commit

--- src/main/java/package1/SaveLoad.java
+++ src/main/java/package1/SaveLoad.java
@@ -1,38 +1,12 @@
 package package1;
 
 import com.example.victorytransport.*;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class SaveLoad {
-    public void saveWhichLevel(String levelName) {
-        File saveFile = new File("whichLevel.txt");
-        try {
-            FileWriter fileWriter = new FileWriter(saveFile);
-            fileWriter.write(levelName);
-            fileWriter.close();
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-    public void saveActions(Text actionsText) {
-        File saveFile = new File("ActionText.txt");
-        try {
-            FileWriter fileWriter = new FileWriter(saveFile);
-            fileWriter.write(actionsText.getText());
-            fileWriter.close();
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     public void save(Level level) {
         File saveFile = new File("saveFile.txt");
         try {
@@ -65,19 +39,11 @@ public class SaveLoad {
         }
     }
 
-
     public Level load(){
         File saveFile = new File("saveFile.txt");
         Level level = new Level(saveFile);
         return level;
     }
-    public Text loadActions() {
-        Scanner actionReader = new Scanner("ActionText.txt");
-        Text actionsText = new Text();
-        if (actionReader.hasNextLine()) {
-            actionsText.setText(actionsText.getText() +"\n"+ actionReader.nextLine());
-        }
-        return actionsText;
-    }
+
 }
 
